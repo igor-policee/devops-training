ee0daf993a8362ff218796fb7f5437c6e891ebb2
Igor Polishchuk <igor.policee@gmail.com>
2022-10-14 15:28:46 +0300

    Add/Edit ansible, terraform files

---
+++ devops-engineer-basics/6-infrastructure-as-code/terraform-config/vm-lb-ubuntu-01.tf
@@ -0,0 +1,26 @@
+resource "yandex_compute_instance" "vm-lb-ubuntu-01" {
+  name = "vm-lb-ubuntu-01"
+
+  resources {
+    cores         = 2
+    memory        = 2
+    core_fraction = 20
+  }
+
+  boot_disk {
+    initialize_params {
+      image_id = var.ubuntu_image_id
+      size     = var.disk_size
+      type     = var.disk_type
+    }
+  }
+
+  network_interface {
+    subnet_id = yandex_vpc_subnet.subnet-1.id
+    nat       = true
+  }
+
+  metadata = {
+    ssh-keys = "ubuntu:${file(var.ssh_public_key)}"
+  }
+}
