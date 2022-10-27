74e944e8356b56e56f37e747bbc6d85bce76e712
Igor Polishchuk <igor.policee@gmail.com>
2022-10-27 19:58:40 +0300

    Add/Edit/Remove files

--- devops-engineer-basics/8-docker/docker-nginx-python2-db-redis-homework/web/wsgi.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python3
-# -*- encoding: utf-8 -*-
-
-def application(env, start_response):
-    start_response('200 OK', [('Content-Type','text/html')])
-    return [b"<h1>Hello Skillbox</h1>"]
