4fb7290525190a0663076a0db1a4c382be9dbd19
Igor Polishchuk <igor.policee@gmail.com>
2022-10-30 15:53:50 +0300

    Edit output.tf

--- devops-engineer-basics/9-ansible/terraform-config/output.tf
+++ devops-engineer-basics/9-ansible/terraform-config/output.tf
@@ -1,7 +1,11 @@
-output "internal_ip_address__" {
-  value = yandex_compute_instance.__.network_interface.0.ip_address
+output "external_ip_address_build-upstream-node-01" {
+  value = yandex_compute_instance.build-upstream-node-01.network_interface.0.nat_ip_address
 }
 
-output "external_ip_address__" {
-  value = yandex_compute_instance.__network_interface.0.nat_ip_address
+output "internal_ip_address_build-upstream-node-01" {
+  value = yandex_compute_instance.build-upstream-node-01.network_interface.0.ip_address
+}
+
+output "internal_ip_address_webserver-node-01" {
+  value = yandex_compute_instance.webserver-node-01.network_interface.0.ip_address
 }
