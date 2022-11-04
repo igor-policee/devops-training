fcea4e808768737770c82b237e090b97ec6b0254
Igor Polishchuk <igor.policee@gmail.com>
2022-11-05 01:37:00 +0300

    Remove docker-phpfpm-nginx/

--- devops-engineer-basics/10-testing-infra-code/docker-phpfpm-nginx/test/test_infra.py
+++
@@ -1,9 +0,0 @@
-def test_system_info(host):
-    assert host.system_info.type == "linux"
-    assert host.system_info.distribution == "alpine"
-
-
-def test_nginx_is_installed(host):
-    nginx = host.package("nginx")
-    assert nginx.is_installed
-    assert nginx.version.startswith("1.1")
